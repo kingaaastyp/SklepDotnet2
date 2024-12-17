68c4b21d32919b7cfa58753903a66277c9883c6f
kingaaastyp <k.stypulkowska5@gmail.com>
2024-12-17 22:09:08 +0100

    Dzialajaca strona

---
+++ Migrations/20241216231213_LOGiHAS.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class LOGiHAS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Haslo",
+                table: "Uzytkownicy",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "Uzytkownicy",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Haslo",
+                table: "Uzytkownicy");
+
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "Uzytkownicy");
+        }
+    }
+}
